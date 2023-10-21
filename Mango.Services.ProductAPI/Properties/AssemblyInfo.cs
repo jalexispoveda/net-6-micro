363de1f6a1fae3afca24ed3de0f407a9a85df64b
jorgepoveda-dev <jalexispoveda010@gmail.com>
2023-10-21 00:33:13 -0500

    test

--- Mango.Services.ProductAPI/Properties/AssemblyInfo.cs
+++ Mango.Services.ProductAPI/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 //[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
-//[assembly: AssemblyInformationalVersion("1.0.0-beta+abcd1234ad")] 
+//[assembly: AssemblyInformationalVersion("1.0.0-beta+abcd123s4ad")] 
 
 
