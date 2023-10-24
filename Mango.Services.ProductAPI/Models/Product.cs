f7b55a0c653b03533ec3674b8963c8e1b07eae47
jorgepoveda-dev <jalexispoveda010@gmail.com>
2023-10-24 15:12:36 -0500

    Nullable prop

--- Mango.Services.ProductAPI/Models/Product.cs
+++ Mango.Services.ProductAPI/Models/Product.cs
@@ -7,7 +7,7 @@ namespace Mango.Services.ProductAPI.Models
         [Key]
         public int ProductId { get; set; }
         [Required]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         [Range(1,1000)]
         public double Price { get; set; }
         public string Description { get; set; }
