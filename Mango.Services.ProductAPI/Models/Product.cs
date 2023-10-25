ac21a1d7d427a79f27a5c2160dc9b389fefd5106
jorgepoveda-dev <jalexispoveda010@gmail.com>
2023-10-24 19:15:29 -0500

    tes

--- Mango.Services.ProductAPI/Models/Product.cs
+++ Mango.Services.ProductAPI/Models/Product.cs
@@ -10,7 +10,7 @@ namespace Mango.Services.ProductAPI.Models
         public string Name { get; set; }
         [Range(1,1000)]
         public double Price { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
     }
